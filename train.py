857f8a819e4a2e3a104714418ef85ada1e3151e6
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-09-25 16:53:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -141,13 +141,3 @@ for epoch in tqdm.tqdm(range(args.epoch), desc='Epoch'):
     os.makedirs(save_dir, exist_ok=True)
     dataset_name = os.path.split(args.data_config)[-1].split('.')[0]
     torch.ave(model.state_dict(), os.path.join(save_dir, 'yolov3_{}_{}.pth'.format(dataset_name, epoch)))
-
-
-
-
-
-
-
-
-
-
